4e6f9323b77d3cd4cd52913c39465a6efa2560d8
Leah Migacz <lmigacz@msudenver.edu>
2020-09-10 16:55:52 -0600

    Everything working correctly

--- src/com/fancystacks/Main.java
+++ src/com/fancystacks/Main.java
@@ -8,13 +8,14 @@ public class Main {
     public static void main(String[] args) {
         final byte MONTHS_OF_YEAR = 12;
         final byte PERCENT = 100;
-
+// variables declared for use outside of functions
         int principal = 0;
         float monthlyInterest = 0;
         int numberOfPayments = 0;
 
         Scanner scanner = new Scanner(System.in);
 
+        // Principal amount if user enters valid number
         while (true) {
             System.out.print("Principal Amount (1k to 1M): ");
             principal = scanner.nextInt();
@@ -23,6 +24,7 @@ public class Main {
             System.out.println("Please enter a value between 1000 and 1000000");
         }
 
+        // Interest rate calculated upon validation
         while (true) {
             System.out.print("Annual Interest Rate: ");
             float annualInterest = scanner.nextFloat();
@@ -33,6 +35,7 @@ public class Main {
             System.out.println("Please enter a value between 1 and 30 ");
         }
 
+        // Payment period calculation upon value entered
         while (true) {
             System.out.print("Period (In Years): ");
             byte years = scanner.nextByte();
